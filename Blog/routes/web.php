a694f607d625580826fe330aa5c8df3365de1884
L9029 <angeloca03@gmail.com>
2024-04-02 19:02:51 -0400

    Installing the laravel/breeze package for the logging system of the project. Besides setting the links of the logging system and the admin dashboard.

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController; //Se llama al controlador PageController que maneja la logica de el Blog
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -32,3 +34,16 @@ Route::controller(PageController::class)->group(function (){
     Route::get('blog', "blog")->name("blog");
     Route::get('blog/{post:slug}', "post")->name("post"); //El parametro slug pasa a ser una propiedad de la tabla posts
 });
+
+//Loging Routes
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

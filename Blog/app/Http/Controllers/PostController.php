67b5f065973e00cc6b3594f30ba803c0b2c53ea0
L9029 <angeloca03@gmail.com>
2024-04-04 14:58:25 -0400

    Showing the Posts in the view.

--- Blog/app/Http/Controllers/PostController.php
+++ Blog/app/Http/Controllers/PostController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(){
-        return view("posts.index"); //Llama a la vista que muestra el listado de publicaciones
+        return view("posts.index", [
+            "posts" => Post::latest()->paginate(),
+        ]); //Llama a la vista y muestra el listado de publicaciones
     }
 }

1630a0994dce40eecb893e93168ead33851d4f1c
L9029 <angeloca03@gmail.com>
2024-04-04 14:03:33 -0400

    Deleting the routes that are not necessary for the project and modifying the navigation view.

--- Blog/routes/auth.php
+++ Blog/routes/auth.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\PasswordController;
 use Illuminate\Support\Facades\Route;
 
 //Ruta del sistema de inicio de sesion
@@ -14,14 +12,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    //Ruta del perfil para el cambio de contrseña
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
     //Ruta para la salida del usuario
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -40,8 +40,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-});
+
 
 require __DIR__.'/auth.php';

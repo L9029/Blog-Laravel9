40073dc6b815fa5f334c242861558d448fa0debd
L9029 <angeloca03@gmail.com>
2024-04-08 14:55:29 -0400

    Rewriting the method home of the PostController that now can paginate the posts of the blog, building the home view and adding some styles, besides developing the logic in the view to show all the posts.

--- Blog/app/Http/Controllers/PageController.php
+++ Blog/app/Http/Controllers/PageController.php
@@ -8,24 +8,25 @@ use App\Models\Post;
 class PageController extends Controller
 {
     public function home(){
-        return view("home");
-    }
-
-    public function blog(){
-        //Consulta a Base de datos
-        //$posts = Post::get(); //Lista todos los posts en la vista
-
-        // $posts = Post::first(); //Trae el primer registro de la tabla posts
-        // dd($posts);
-        
-        // $post = Post::find(71); //Trae un Registro especificado por su id de la tabla posts
-        // dd($post);
-
-        //Se consultan los registros en orden descendente
         $posts = Post::first()->paginate(); //Con paginate se paginan los registros
-
-        return view("blog", ["posts" => $posts]);
+        return view("home", ["posts" => $posts]);
     }
+
+    // public function blog(){
+    //     //Consulta a Base de datos
+    //     //$posts = Post::get(); //Lista todos los posts en la vista
+
+    //     // $posts = Post::first(); //Trae el primer registro de la tabla posts
+    //     // dd($posts);
+        
+    //     // $post = Post::find(71); //Trae un Registro especificado por su id de la tabla posts
+    //     // dd($post);
+
+    //     //Se consultan los registros en orden descendente
+    //     $posts = Post::first()->paginate(); //Con paginate se paginan los registros
+
+    //     return view("blog", ["posts" => $posts]);
+    // }
 
     public function post(Post $post){
 
--- Blog/resources/views/home.blade.php
+++ Blog/resources/views/home.blade.php
@@ -1,4 +1,26 @@
 @extends("template")
 @section("content")
-    <h1>Home</h1>
+<div>
+    <!-- Informacion Destacada -->
+</div>
+
+<!-- Paginacion de los Posts en el Home -->
+<div class="px-4">
+    <h1 class="text-2xl mb-8 text-gray-900">Contenido Tecnico</h1>
+
+    <div class="grid grid-cols-1 gap-4 mb-4">
+        @foreach($posts as $post)
+        <a href="" class="bg-gray-200 rounded-lg px-6 py-4">
+            <p class="text-xs flex items-center gap-2">
+                <span class="uppercase text-gray-900 bg-gray-400 rounded-full px-2 py-1">Tutorial</span>
+                <span class="text-gray-900 bg-gray-300 rounded-full px-2 py-1">{{ $post->created_at->format('d/m/Y') }}</span>
+            </p>
+
+            <h2 class="text-lg text-gray-950 mt-2">{{ $post->title }}</h2>
+        </a>
+        @endforeach
+    </div>
+    {{ $posts->links() }}
+</div>
+
 @endsection

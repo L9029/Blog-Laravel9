90e71eb99fcf3129639efea5594cb637ccef347d
L9029 <angeloca03@gmail.com>
2024-04-08 12:25:01 -0400

    Creating the images folder and adding some images. Building some styles in the template page.

--- Blog/resources/views/template.blade.php
+++ Blog/resources/views/template.blade.php
@@ -4,24 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <main>
         <section>
-            <nav>
-                <a href="{{ route('home') }}">Home</a>
-                <a href="{{ route('blog') }}">Blog</a>
+            <div class="container px-4 mx-auto">
+                <header class="flex justify-between items-center py-4">
+                    <div class="flex items-center flex-grow gap-4">
+                        <a href="{{ route('home') }}">
+                            <img src="{{ asset('images/logo.png') }}" class="h-12">
+                        </a>
 
-                @auth
-                <a href="{{ route('dashboard') }}">Dashboard</a>
-                @else
-                <a href="{{ route('login') }}">Login</a>
-                @endauth
-            </nav>
+                        <form action="">
+                            <input type="text" placeholder="Buscar">
+                        </form>
+                    </div>
 
-            <hr>
-
-            @yield("content") 
+                    @auth
+                    <a href="{{ route('dashboard') }}">Dashboard</a>
+                    @else
+                    <a href="{{ route('login') }}">Login</a>
+                    @endauth
+                </header>
+                
+                <hr>
+                @yield("content") 
+            </div>
         </section>
     </main>
 </body>
